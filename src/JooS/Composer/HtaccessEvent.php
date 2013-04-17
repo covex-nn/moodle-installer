e4795e2c8ec95faa749af03c92a1d285ef5fbf67
Andrey F. Mindubaev <covex.mobile@gmail.com>
2013-04-17 12:59:59 +0400

    Put 'Deny from all' to vendor/.htaccess file

---
+++ src/JooS/Composer/HtaccessEvent.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace JooS\Composer;
+
+use Composer\Script\Event;
+
+class HtaccessEvent
+{
+  
+  /**
+   * Create .htaccess and write 'Deny from all'
+   * 
+   * @param Event $event
+   * 
+   * @return null
+   */
+  public static function denyFromAll(Event $event)
+  {
+    $composer = $event->getComposer();
+    $path = $composer->getConfig()->get("vendor-dir");
+
+    $htaccess = new self($path);
+    $htaccess->append("Deny from all");
+    
+    $composerIo = $event->getIO();
+    $composerIo->write(".htaccess created in vendor-dir");
+  }
+  
+  /**
+   * @var string
+   */
+  private $_path;
+  
+  /**
+   * Public functructor
+   * 
+   * @param string $path Path to .htaccess
+   */
+  public function __construct($path)
+  {
+    $this->_setPath($path);
+  }
+
+  /**
+   * 
+   * @param type $string
+   * 
+   * @return boolean
+   * @todo сделать более интеллектуальненько =)
+   */
+  public function append($string)
+  {
+    $array = $this->getContentsArray();
+    if (!in_array($string, $array)) {
+      $array[] = $string;
+    }
+    return $this->_write($array);
+  }
+  
+  /**
+   * Write contents to file
+   * 
+   * @param string $contents Contents
+   * 
+   * @return boolean
+   */
+  protected function _write($contents)
+  {
+    if (is_array($contents)) {
+      $contents = implode(PHP_EOL, $contents) . PHP_EOL;
+    } else {
+      $contents = trim($contents) . PHP_EOL;
+    }
+    if ($this->writable()) {
+      $path = $this->getPath();
+      $result = !!file_put_contents($path, $contents);
+    } else {
+      $result = false;
+    }
+    
+    return $result;
+  }
+  
+  /**
+   * Return file contents
+   * 
+   * @return string
+   */
+  public function getContents()
+  {
+    $path = $this->getPath();
+    if ($this->exists()) {
+      $contents = file_get_contents($path);
+    } else {
+      $contents = "";
+    }
+    return $contents;
+  }
+  
+  public function getContentsArray()
+  {
+    $contents = $this->getContents();
+    
+    $lines = explode(PHP_EOL, $contents);
+    foreach ($lines as $key => $value) {
+      $value = trim($value);
+      if (strlen($value)) {
+        $lines[$key] = $value;
+      } else {
+        unset($lines[$key]);
+      }
+    }
+    return array_values($lines);
+  }
+  
+  /**
+   * Return if file exists
+   * 
+   * @return boolean
+   */
+  public function exists()
+  {
+    $path = $this->getPath();
+    return file_exists($path);
+  }
+  
+  /**
+   * Return if file writable
+   * 
+   * @return boolean
+   */
+  public function writable()
+  {
+    $path = $this->getPath();
+    if ($this->exists()) {
+      $writable = is_writable($path);
+    } else {
+      $dir = dirname($path);
+      $writable = is_writable($dir);
+    }
+    return $writable;
+  }
+  
+  /**
+   * Return path to .htaccess
+   * 
+   * @return string
+   */
+  public function getPath()
+  {
+    return $this->_path;
+  }
+  
+  /**
+   * Set path to .htaccess
+   * 
+   * @param string $path Path to .htaccess
+   * 
+   * @return HtaccessEvent
+   */
+  protected function _setPath($path)
+  {
+    $this->_path = $path;
+    
+    return $this;
+  }
+  
+}
---
+++ tests/src/JooS/Composer/HtaccessEventTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace JooS\Composer;
+
+use JooS\Stream\Wrapper_FS;
+
+class HtaccessEventTest extends \PHPUnit_Framework_TestCase
+{
+  
+  /**
+   * @var HtaccessEvent
+   */
+  private $_htaccess;
+  
+  /**
+   * Test contents
+   * 
+   * @return null
+   */
+  public function testEmptyContent()
+  {
+    $path = $this->_htaccessPath();
+    
+    $this->assertEquals($path, $this->_htaccess->getPath());
+    $this->assertTrue(!$this->_htaccess->exists());
+    $this->assertTrue($this->_htaccess->writable());
+    $this->assertEquals("", $this->_htaccess->getContents());
+    $this->assertEquals(array(), $this->_htaccess->getContentsArray());
+    
+    $denyFromAll = "Deny from all";
+    $expected = array($denyFromAll);
+
+    file_put_contents($path, $denyFromAll);
+    $this->assertEquals($expected, $this->_htaccess->getContentsArray());
+
+    file_put_contents($path, $denyFromAll . "\n");
+    $this->assertEquals($expected, $this->_htaccess->getContentsArray());
+  }
+  
+  public function testAppend()
+  {
+    $denyFromAll = "Deny from all";
+    $this->_htaccess->append($denyFromAll);
+    $this->assertEquals($denyFromAll . PHP_EOL, $this->_htaccess->getContents());
+
+    $this->_htaccess->append($denyFromAll);
+    $this->assertEquals($denyFromAll . PHP_EOL, $this->_htaccess->getContents());
+  }
+  
+  /**
+   * Sets up the fixture, for example, open a network connection.
+   * 
+   * This method is called before a test is executed.
+   *
+   * @return null
+   */
+  protected function setUp()
+  {
+    Wrapper_FS::register("htaccess");
+    mkdir("htaccess://folder");
+    
+    $this->_htaccess = new HtaccessEvent(
+      $this->_htaccessPath()
+    );
+  }
+  
+  /**
+   * Tears down the fixture, for example, close a network connection.
+   * 
+   * This method is called after a test is executed.
+   * 
+   * @return null
+   */
+  protected function tearDown()
+  {
+    Wrapper_FS::unregister("htaccess");
+  }
+  
+  protected function _htaccessPath()
+  {
+    return "htaccess://folder/.htaccess";
+  }
+  
+}
