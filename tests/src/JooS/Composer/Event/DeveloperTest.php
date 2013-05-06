8f3c884e43c4152bb9f43951972d0b0e17334d9a
Andrey F. Mindubaev <covex.mobile@gmail.com>
2013-05-06 23:47:45 +0400

    Added unit tests

--- tests/src/JooS/Composer/Event/DeveloperTest.php
+++ tests/src/JooS/Composer/Event/DeveloperTest.php
@@ -13,21 +13,80 @@ class DeveloperTest extends \PHPUnit_Framework_TestCase
 {
   
   /**
-   * Test event for developers
+   * Test events create/remove symlinks
    * 
    * @return null
    */
-  public function testCreateSymlinks()
+  public function testCreateRemoveSymlinks()
   {
-    $files = new Files();
-    $root = $files->mkdir();
-    
-    $link = $root . "/test/folder";
+    $root = $this->_files->mkdir();
+    $folder = "test/folder";
     $target = __DIR__;
     
+    $event = $this->_getEventMock($root, $folder, $target);
+    
+    Developer::createSymlinks($event);
+    
+    $link = $root . "/" . $folder;
+    
+    $this->assertFileExists($link);
+    $this->assertTrue(is_link($link));
+    $this->assertEquals(
+      realpath($target), realpath(readlink($link))
+    );
+    
+    Developer::removeSymlinks($event);
+    
+    $this->assertFileNotExists($link);
+  }
+  
+  public function testSetConfigContent()
+  {
+    Developer::setConfigContent(null);
+    $this->assertEquals(null, Developer::getConfigContent());
+    Developer::setConfigContent("qwerty");
+    $this->assertEquals("qwerty", Developer::getConfigContent());
+    Developer::setConfigContent(null);
+  }
+  
+  public function testSaveRestoreConfig()
+  {
+    $root = $this->_files->mkdir();
+    $folder = "test/folder";
+    $target = __DIR__;
+    
+    $event = $this->_getEventMock($root, $folder, $target);
+    
+    $configPhp = $root . "/config.php";
+    
+    Developer::saveConfig($event);
+    $this->assertEquals(null, Developer::getConfigContent());
+    Developer::restoreConfig($event);
+    
+    $this->assertFileNotExists($configPhp);
+    
+    file_put_contents($configPhp, "qwerty");
+    Developer::saveConfig($event);
+    $this->assertEquals("qwerty", Developer::getConfigContent());
+    
+    unlink($configPhp);
+    Developer::restoreConfig($event);
+    
+    clearstatcache();
+    $this->assertFileExists($configPhp);
+    $this->assertEquals("qwerty", file_get_contents($configPhp));
+  }
+  
+  /**
+   * Return Event Mock object
+   * 
+   * @return Event
+   */
+  private function _getEventMock($root, $folder, $target)
+  {
     $extra = array(
       MoodleInstaller::MOODLE_MODULES => array(
-        "test/folder" => $target
+        $folder => $target
       )
     );
     
@@ -54,13 +113,32 @@ class DeveloperTest extends \PHPUnit_Framework_TestCase
     $io = $this->getMock("Composer\\IO\\IOInterface");
     $event = new Event("post-update-cmd", $composer, $io);
     
-    Developer::createSymlinks($event);
-    
-    $this->assertFileExists($link);
-    $this->assertTrue(is_link($link));
-    $this->assertEquals(
-      realpath($target), realpath(readlink($link))
-    );
+    return $event;
+  }
+  
+  /**
+   * @var Files
+   */
+  private $_files = null;
+  
+  /**
+   * Sets up the fixture
+   * 
+   * @return null
+   */
+  protected function setUp()
+  {
+    $this->_files = new Files();
+  }
+  
+  /**
+   * Tears down the fixture
+   * 
+   * @return null
+   */
+  protected function tearDown()
+  {
+    unset($this->_files);
   }
   
 }
